0c346c2508bb9d29aadb2017ba9cdedf56c28ea6
zibonbadi <zibonbadi@users.noreply.github.com>
2023-07-01 00:33:35 +0200

    Fix debug URL

--- ll_info.py
+++ ll_info.py
@@ -1,7 +1,6 @@
 product_name = "LiquidLauncher"
 product_version = "v1.0"
-#version_check_url = "https://api.github.com/repos/liquidunderground/liquidlauncher/releases/latest"
-version_check_url = "file:///home/martin/git/liquidlauncher/version_test.json"
+version_check_url = "https://api.github.com/repos/liquidunderground/liquidlauncher/releases/latest"
 
 http_headers = {'User-Agent': '{}/{}'.format(product_name, product_version) }
 
