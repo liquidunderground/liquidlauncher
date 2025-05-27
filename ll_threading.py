a857ee820f5e19a1dd534f62d36166cdb2bf738c
zibonbadi <zibonbadi@users.noreply.github.com>
2025-05-27 20:48:37 +0200

    Remove hardcoded URL path from snitch API

--- ll_threading.py
+++ ll_threading.py
@@ -125,7 +125,7 @@ class QueryLiquid(QtCore.QThread):
                     print('Snitching to "{}"...\n'.format(self.snitch_dest))
                     self.update_snitchmsg_sig.emit('Snitching to "{}"...'.format(self.snitch_dest))
                     snitch_csv_obj = {"file": ('snitch.csv', snitch_csv_txt) }
-                    res = requests.post(self.snitch_dest.rstrip("/")+"/liquidms/snitch", headers=http_headers, files=snitch_csv_obj) # Pass CSV to Snitch API
+                    res = requests.post(self.snitch_dest.rstrip("/"), headers=http_headers, files=snitch_csv_obj) # Pass CSV to Snitch API
                     res.raise_for_status()
                     self.update_snitchmsg_sig.emit('Successfully snitched {} to {}.  Thank you.'.format(self.snitch_src, self.snitch_dest))
                 except Exception as e:
