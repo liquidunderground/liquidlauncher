5ae2f08d793a69f0baf5233236784441e486501b
zibonbadi <zibonbadi@users.noreply.github.com>
2025-12-06 21:50:26 +0100

    NetgameDialog: Fix netgame modsource check

--- ui/netgamedialog.py
+++ ui/netgamedialog.py
@@ -19,7 +19,7 @@ class NetgameDialog(QtWidgets.QDialog):
 
         # UI setup
         self.ui.FilesTable.addAction(self.parent().qicons["copy"], "Copy MD5 to clipboard", lambda: self.file_copy_md5(self.ui.FilesTable.currentRow()))
-        if self.parent().global_settings["modsources"]["gameserver"]:
+        if "netgame" in self.parent().global_settings["modsources"]:
             self.ui.FilesTable.addAction(self.parent().qicons["download"], "Download", lambda: self.download_file(self.ui.FilesTable.currentRow()))
 
         ll_signalbus.netgame_update_finish.connect(self.refresh)
